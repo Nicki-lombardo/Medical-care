b398cb630d75ad7ad8f99323965c0d6a33690be6
MONICA <monik.ferrer@gmail.com>
2024-09-25 17:19:37 +0200

    instale dependencias de global style y tailwind

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,5 @@
 import type { Metadata } from "next";
-//import { Plus_Jakarta_Sans } from "next/font/local"; // no funciona cuando cambio la fuente Plus_Jakarta..
 import { Plus_Jakarta_Sans } from "next/font/google";
-
 import "./globals.css";
 import { cn } from "@/lib/utils";
 
