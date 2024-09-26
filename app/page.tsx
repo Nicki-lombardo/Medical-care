667baf58a0473de184eb43509fea80e9becb8bfd
MONICA <monik.ferrer@gmail.com>
2024-09-26 19:36:46 +0200

    Cree el folder forms e inclui el componente PatientForm

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import PatientForm from "@/components/forms/PatientForm";
 import Image from "next/image";
 
 export default function Home() {
@@ -5,16 +6,16 @@ export default function Home() {
     <div className="flex h-screen max-h-screen">
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
-          <image
+          <Image
             src="/assets/icons/logo-full.svg"
             height={1000}
             width={1000}
             alt="patient"
             className="mb-12 h-10 w-fit"
           />
+          <PatientForm />
         </div>
       </section>
     </div>
   );
 }
-
