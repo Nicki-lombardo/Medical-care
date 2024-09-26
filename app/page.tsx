87302ec918e06a4c95cf0514bba6d74e1cefcbb2
MONICA <monik.ferrer@gmail.com>
2024-09-26 17:43:57 +0200

    importe el componente theme-provider de shacn para que el boton se active click

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div>
       <h1 className="text-white -3xl underline">Homepage Medical-care</h1>
+      <Button>Click me</Button>
     </div>
   );
 }
